e75cd2b3caeeade4000b8460e68d29df7b9c84fb
nabilcoy132-lab <nabilcoy132@gmail.com>
2026-08-12 11:40:51 +0700

    Create competitor.js

---
+++ js/competitor.js
@@ -0,0 +1,11 @@
+window.ANALSYTICI_COMPETITOR = {
+  competitors: [],
+
+  init() {
+    console.log("Competitor Intelligence module loaded");
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  window.ANALSYTICI_COMPETITOR.init();
+});
