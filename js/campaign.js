14480f8e4f7b0c505aa2fbb9447bdf20dfe192c7
nabilcoy132-lab <nabilcoy132@gmail.com>
2026-08-12 11:42:12 +0700

    Create campaign.js

---
+++ js/campaign.js
@@ -0,0 +1,11 @@
+window.ANALSYTICI_CAMPAIGN = {
+  campaigns: [],
+
+  init() {
+    console.log("Campaign Intelligence module loaded");
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  window.ANALSYTICI_CAMPAIGN.init();
+});
