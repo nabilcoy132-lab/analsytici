4cdec16c7d423bd5e9641e0383e50ac3a893c6ad
nabilcoy132-lab <nabilcoy132@gmail.com>
2026-08-12 11:39:45 +0700

    Create research.js

---
+++ js/research.js
@@ -0,0 +1,11 @@
+window.ANALSYTICI_RESEARCH = {
+  projects: [],
+
+  init() {
+    console.log("Research module loaded");
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  window.ANALSYTICI_RESEARCH.init();
+});
