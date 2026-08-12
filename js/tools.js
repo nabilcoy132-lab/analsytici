252ada6bb88c69a31f137d9f6cfea2af89ebe8b6
nabilcoy132-lab <nabilcoy132@gmail.com>
2026-08-12 11:45:14 +0700

    Create tools.js

---
+++ js/tools.js
@@ -0,0 +1,11 @@
+window.ANALSYTICI_TOOLS = {
+  tools: [],
+
+  init() {
+    console.log("Marketing Tools module loaded");
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  window.ANALSYTICI_TOOLS.init();
+});
