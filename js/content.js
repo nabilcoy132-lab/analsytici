cc1de6349add36d9c1ce9028da08ac118c49bd2e
nabilcoy132-lab <nabilcoy132@gmail.com>
2026-08-12 11:41:27 +0700

    Create content.js

---
+++ js/content.js
@@ -0,0 +1,11 @@
+window.ANALSYTICI_CONTENT = {
+  contents: [],
+
+  init() {
+    console.log("Content Intelligence module loaded");
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  window.ANALSYTICI_CONTENT.init();
+});
