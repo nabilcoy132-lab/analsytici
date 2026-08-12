65ed35524d34064d0c441debdbe36e4e0a479dbd
nabilcoy132-lab <nabilcoy132@gmail.com>
2026-08-12 11:44:11 +0700

    Create strategy.js

---
+++ js/strategy.js
@@ -0,0 +1,11 @@
+window.ANALSYTICI_STRATEGY = {
+  outputs: [],
+
+  init() {
+    console.log("Strategy module loaded");
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  window.ANALSYTICI_STRATEGY.init();
+});
